08aa07161e34f4e39aff57b77e77ddc0445a7730
Marcele Montalvão <mmontalvaoblog@outlook.com>
2023-02-13 16:13:34 -0300

    fix: customers endpoints again

--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import { getAllCustomers, getCustomersById, postCustomer, putCustomer } from '../controllers/customersController.js'
-import { validateCustomerSchema } from '../middlewares/customersMiddleware.js'
+import { validateCustomerSchema, validatePutCustomerSchema } from '../middlewares/customersMiddleware.js'
 
 const customersRouters = Router()
 
 customersRouters.get('/customers', getAllCustomers)
 customersRouters.get('/customers/:id', getCustomersById)
 customersRouters.post('/customers', validateCustomerSchema, postCustomer)
-customersRouters.put('/customers/:id', validateCustomerSchema, putCustomer)
+customersRouters.put('/customers/:id', validatePutCustomerSchema, putCustomer)
 
 export default customersRouters
